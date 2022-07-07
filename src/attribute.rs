8705bc8536394a11edf3a3d022c63e30ab8dac33
Zakhar Semenov <deadlion24@gmail.com>
2022-07-07 14:55:51 +0600

    unit test

--- src/attribute.rs
+++ src/attribute.rs
@@ -10,7 +10,36 @@ pub struct KaspiCategoryAttribute {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum Value {
+    String(String),
+    Boolean(bool)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct AttributeValue {
     pub code: String,
-    pub value: String,
+    pub value: Value,
+}
+
+mod tests {
+    #[test]
+    fn attribute_to_json() {
+        use super::{AttributeValue, Value};
+
+        let attribute = AttributeValue {
+            code: String::from("ASD"),
+            value: Value::String(String::from("ASD"))
+        };
+
+        let json = serde_json::to_value(attribute).unwrap();
+
+        assert_eq!(
+            json,
+            serde_json::json!({
+                "code": "ASD",
+                "value": "ASD"
+            })
+        );
+    }
 }
