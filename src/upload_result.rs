759f0b978f33a0e0ba26de7974789b63a4ff8207
Zakhar Semenov <deadlion24@gmail.com>
2022-07-08 16:59:06 +0600

    upload status struct

--- src/upload_result.rs
+++ src/upload_result.rs
@@ -1,6 +1,12 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct UploadStatus {
+    code: String,
+    status: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct UploadResult {
     errors: Option<Vec<String>>,
     warnings: Option<Vec<String>>,
